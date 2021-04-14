1cc06c9763655027496d1a1621871cf513de8ec0
MarkWengSTR <bskin0330@gmail.com>
2021-04-14 10:32:21 +0800

    Render data

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Product, Order, Customer
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    return render(request, 'accounts/dashboard.html',
+                  {
+                      'orders': orders,
+                      'customers': customers,
+                      'total_customers': total_customers,
+                      'total_orders': total_orders,
+                      'pending': pending,
+                      'delivered': delivered,
+                  })
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+
+    return render(request, 'accounts/products.html', {'products': products})
 
 
 def customer(request):
