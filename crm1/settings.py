49dad62065100345b5bf8439fb0caeb43a0ef48c
MarkWengSTR <bskin0330@gmail.com>
2021-04-16 14:28:58 +0800

    Password Reset email

--- crm1/settings.py
+++ crm1/settings.py
@@ -139,6 +139,15 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
+# SMTP Configuration
+EMAIL_BACKEND = 'django.core.email.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'bskin0330@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+# need set security to "Less" if use Gmail
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
